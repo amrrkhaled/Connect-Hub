8b704bc120aba66c5a09fb7fcb6cc013a175e126
AmrrKhaled <amr.khaledsaada@gmail.com>
2024-12-04 04:06:49 +0200

    Fixed User bug

--- src/main/java/backend/jsonExample.java
+++ src/main/java/backend/jsonExample.java
@@ -4,18 +4,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class jsonExample {
-    public static void main(String[] args) {
-       String username = "ahmed";
-       String password ="12345467";
-       String dof ="2004-10-12";
-       String email ="a.samehmepsn@gfmx.com";
-       ILoadUsers loadUsers = new LoadUsers();
-       IAddUser user = new AddUser(loadUsers);
-       Validation valid = new UserValidator(loadUsers);
-       IUpdateUser updateUser = new UpdateUser();
-       UserManager manager = new UserManager(user , loadUsers , valid , updateUser);
-       manager.logout(username);
-       String msg = manager.login(username, password);
-       System.out.println(msg);
-    }
+   public static void main(String[] args) {
+      String username = "ahmed";
+      String password ="12345467";
+      String dof ="2004-10-12";
+      String email ="a.samehmepsn@gfmx.com";
+      ILoadUsers loadUsers = new LoadUsers();
+      IAddUser user = new AddUser(loadUsers);
+      Validation valid = new UserValidator(loadUsers);
+      IUpdateUser updateUser = new UpdateUser();
+      UserManager manager = new UserManager(user , loadUsers , valid , updateUser);
+      manager.logout(username);
+      String msg = manager.login(username, password);
+      System.out.println(msg);
+   }
 }
