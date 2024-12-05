d7a7fad34ad77a5f57a24e7bd45ff3f89fcffa2e
ahmed <a.samehpsn@gmail.com>
2024-12-05 18:30:54 +0200

    frontend of accountManagement worked.

--- src/main/java/frontend/Signup.java
+++ src/main/java/frontend/Signup.java
@@ -9,14 +9,14 @@ public class Signup extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Signup.class.getResource("Signup.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        FXMLLoader fxmlLoader = new FXMLLoader(Signup.class.getResource("signup.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),800,700);
         primaryStage.setTitle("Signup");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
