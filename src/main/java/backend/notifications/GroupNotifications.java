f9defabb72277214b07cf9a11025f458c9a666a7
ahmed <a.samehpsn@gmail.com>
2024-12-13 01:20:29 +0200

    added groupNotifications.

--- src/main/java/backend/notifications/GroupNotifications.java
+++ src/main/java/backend/notifications/GroupNotifications.java
@@ -1,4 +1,39 @@
 package backend.notifications;
 
-public class GroupNotifications {
+import backend.Groups.ILoadGroups;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class GroupNotifications implements IGroupNotifications{
+    ILoadNotifications loadNotifications;
+    private final String FILEPATH = "data/groupNotifications.json";
+    public GroupNotifications(ILoadNotifications loadNotifications) {
+        this.loadNotifications = loadNotifications;
+    }
+    public void createNotifications(String groupId, String postId, String timestamp){
+        JSONObject newNotification = new JSONObject();
+        JSONArray notifications = loadNotifications.LoadNotification(FILEPATH);
+        newNotification.put("groupId", groupId);
+        newNotification.put("postId", postId);
+        newNotification.put("timestamp", timestamp);
+        notifications.put(newNotification);
+        loadNotifications.saveNotification(notifications,FILEPATH);
+    }
+    public JSONArray getNotification(){
+        JSONArray notifications = loadNotifications.LoadNotification(FILEPATH);
+        JSONArray groupNotifications = new JSONArray();
+        if (notifications == null) {
+            System.err.println("Error: Notifications file could not be loaded.");
+            return groupNotifications;
+        }
+        for(int i = 0; i < notifications.length(); i++){
+            try{
+                JSONObject notification = notifications.getJSONObject(i);
+                groupNotifications.put(notification);
+            }catch (Exception e) {
+                System.err.println("Error processing Notification at index " + i + ": " + e.getMessage());
+            }
+        }
+        return groupNotifications;
+    }
 }
--- src/main/java/backend/notifications/IGroupNotifications.java
+++ src/main/java/backend/notifications/IGroupNotifications.java
@@ -3,6 +3,7 @@ package backend.notifications;
 import org.json.JSONArray;
 
 public interface IGroupNotifications {
+    public void createNotifications(String groupId, String postId, String timestamp);
     public JSONArray getNotification();
 
 }
